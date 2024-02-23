f04391ade6ee1ea1cd1401d47d07a34c0f56658b
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-02-23 02:36:14 +0100

    validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,64 +1,20 @@
 <template>
     <div>
-        <div>
-            <div>
-                <h1>Log in</h1>
-
-                <form @submit.prevent="submitForm">
-                    <div>
-                        <label>Username or email</label>
-                        <div>
-                            <input type="text" name="username" v-model="username">
-                        </div>
-                    </div>
-
-                    <div>
-                        <label>Password</label>
-                        <div>
-                            <input type="password" name="password" v-model="password">
-                        </div>
-                    </div>
-
-                    <div>
-                        <div>
-                            <button>Log in</button>
-                        </div>
-                    </div>
-                </form>
-
-                <hr>
-
-                <router-link to="/signup">Click here</router-link> to sign up!
-            </div>
-        </div>
+        <h3>Log in</h3>
+        <LoginForm></LoginForm>
+        <router-link to="/signup">Click here</router-link>
+        to sign up!
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import LoginForm from "@/components/LoginForm.vue";
 
 export default {
     name: 'LoginView',
-    data() {
-        return {
-            username: '',
-            password: '',
-            errors: []
-        }
-    },
-    methods: {
-        ...mapActions(['loginUser']), 
-        async submitForm() {
-            try {
-                await this.loginUser({
-                    username: this.username,
-                    password: this.password
-                });
+    components: {
+        LoginForm,
 
-            } catch (error) {
-                console.error('Login failed:', error);
-            }
-        }
     }
 }
 </script>
